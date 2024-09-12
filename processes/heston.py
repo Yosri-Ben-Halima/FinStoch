dd719b6700264c71caa954f00fa5a9ccb23ac28c
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-09-12 16:52:21 +0100

    Update

--- processes/heston.py
+++ processes/heston.py
@@ -7,17 +7,20 @@ from utils.plotting import plot_simulated_paths
 
 class HestonModel: 
 
-    def __init__(self, S0: float, mu: float, sigma: float, theta: float, kappa: float, T: float, num_steps: float, num_paths: float) -> None:
+    def __init__(self, S0: float, mu: float, sigma: float, theta: float, kappa: float, rho: float, T: float, num_steps: float, num_paths: float) -> None:
         
         self._S0 = S0
         self._mu = mu
         self._sigma = sigma
         self._theta = theta
         self._kappa = kappa
+        self._rho = rho
         self._T = T
         self._num_steps = num_steps
         self._num_paths = num_paths
         self._dt = T/num_steps
         self._t = np.linspace(0, T, num_steps)
+    
+    
 
     
