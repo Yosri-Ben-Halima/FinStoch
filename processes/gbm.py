db8a808ef51d6bcbfd77ecafd94e384054013670
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-09-18 01:50:37 +0100

    Update

--- processes/gbm.py
+++ processes/gbm.py
@@ -4,10 +4,11 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.random import generate_random_numbers 
 from utils.plotting import plot_simulated_paths
+from utils.timesteps import generate_date_range
 
 class GeometricBrownianMotion:
 
-    def __init__(self, S0: float, mu: float, sigma: float, T: float, num_steps: float, num_paths: float) -> None:
+    def __init__(self, S0: float, mu: float, sigma: float, T: float, num_steps: float, num_paths: float, start_date: str=None) -> None:
         
         self._S0 = S0
         self._mu = mu
@@ -16,7 +17,10 @@ class GeometricBrownianMotion:
         self._num_steps = num_steps
         self._num_paths = num_paths
         self._dt = T/num_steps
+        self._start_date = start_date
         self._t = np.linspace(0, T, num_steps)
+        if start_date is not None:
+            self._t = generate_date_range(start_date, T, num_steps)
 
     def simulate(self) -> np.ndarray:
         """
@@ -72,7 +76,10 @@ class GeometricBrownianMotion:
     def T(self, value: float) -> None:
         self._T = value
         self._dt = value / self._num_steps
-        self._t = np.linspace(0, value, self._num_steps)
+        if self._start_date is not None:
+            self._t = generate_date_range(self._start_date, value, self._num_steps)
+        else :
+            self._t = np.linspace(0, value, self._num_steps)
 
     @property
     def num_steps(self) -> int:
@@ -82,7 +89,10 @@ class GeometricBrownianMotion:
     def num_steps(self, value: int) -> None:
         self._num_steps = value
         self._dt = self._T / value
-        self._t = np.linspace(0, self._T, value)
+        if self._start_date is not None:
+            self._t = generate_date_range(self._start_date, self._T, value)
+        else :
+            self._t = np.linspace(0, self._T, value)
     
     @property
     def num_paths(self) -> int:
@@ -99,5 +109,13 @@ class GeometricBrownianMotion:
     @property
     def t(self) -> np.ndarray:
         return self._t
-
+    
+    @property
+    def start_date(self) -> np.ndarray:
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, value: str) -> None:
+        self._start_date = value
+        self._t = generate_date_range(value, self._T, self._num_steps)
 
