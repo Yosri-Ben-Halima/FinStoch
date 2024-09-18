9a57a51f070edcca0bdf70c8a7a4deb099ffb761
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-09-18 09:29:42 +0100

    Update

--- utils/random.py
+++ utils/random.py
@@ -1,6 +1,36 @@
 import numpy as np
 
 def generate_random_numbers(distribution: str, size: int, **kwargs) -> np.ndarray:
+    """
+    Generate an array of random numbers based on the specified distribution.
+
+    Parameters
+    ----------
+    distribution : str
+        The type of distribution to use ('normal' or 'poisson').
+    size : int
+        The number of random numbers to generate.
+    **kwargs : Additional parameters for the distribution.
+        For 'normal' distribution:
+            mean : float, optional
+                The mean of the distribution (default is 0).
+            stddev : float, optional
+                The standard deviation of the distribution (default is 1).
+        For 'poisson' distribution:
+            lam : float, optional
+                The lambda (rate) parameter of the distribution (default is 1).
+
+    Returns
+    -------
+    np.ndarray
+        An array of random numbers generated from the specified distribution.
+
+    Raises
+    ------
+    ValueError
+        If the specified distribution type is unsupported.
+    """
+
     if distribution == 'normal':
         return np.random.normal(kwargs.get('mean', 0), kwargs.get('stddev', 1), size)
     elif distribution == 'poisson':
