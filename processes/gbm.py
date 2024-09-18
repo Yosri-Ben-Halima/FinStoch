223052e12c719264312b20cc05d7c778973321b6
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-09-18 09:53:50 +0100

    Update GBM

--- processes/gbm.py
+++ processes/gbm.py
@@ -1,15 +1,57 @@
 import numpy as np 
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.random import generate_random_numbers 
 from utils.plotting import plot_simulated_paths
 from utils.timesteps import generate_date_range
 
 class GeometricBrownianMotion:
+    """
+    A class to model the Geometric Brownian Motion (GBM) process for simulating asset prices.
 
+    Attributes
+    ----------
+    S0 : float
+        The initial asset price.
+    mu : float
+        The drift or mean return of the asset.
+    sigma : float
+        The volatility (standard deviation) of the asset.
+    T : float
+        The time horizon (in years) for the simulation.
+    num_steps : int
+        The number of time steps for the simulation.
+    num_paths : int
+        The number of paths to simulate.
+    dt : float
+        The time increment between steps (T / num_steps).
+    start_date : str, optional
+        The start date for the simulation. If not provided, time is treated numerically.
+    t : np.ndarray
+        The time or date range for the simulation steps.
+    """
     def __init__(self, S0: float, mu: float, sigma: float, T: float, num_steps: float, num_paths: float, start_date: str=None) -> None:
-        
+        """
+        Initialize the parameters for the GBM model and set up the time or date steps.
+
+        Parameters
+        ----------
+        S0 : float
+            The initial asset price.
+        mu : float
+            The drift or mean return rate.
+        sigma : float
+            The volatility or standard deviation of returns.
+        T : float
+            The time horizon (in years).
+        num_steps : int
+            The number of time steps.
+        num_paths : int
+            The number of paths to simulate.
+        start_date : str, optional
+            Starting date for the simulation. If provided, time steps are based on dates. Otherwise, it's numerical.
+        """
         self._S0 = S0
         self._mu = mu
         self._sigma = sigma
@@ -25,13 +67,17 @@ class GeometricBrownianMotion:
 
     def simulate(self) -> np.ndarray:
         """
-        Simulates a path of the Merton jump diffusion model.
+        Simulates a path of the GBM jump diffusion model.
 
-        ## Parameters:
-        - self: Instance of MertonModel class.
+        Parameters
+        ----------
+        self : GeometricBrownianMotion
+            Instance of GeometricBrownianMotion class.
         
-        ## Returns:
-        - A numpy array representing the asset price path.
+        Returns
+        -------
+        np.ndarray
+            A 2D array of shape (num_paths, num_steps), where each row represents a simulated price path.
         """
         S = np.zeros((self._num_paths, self._num_steps))
         S[:, 0] = self._S0
@@ -43,6 +89,16 @@ class GeometricBrownianMotion:
         return S
 
     def plot(self, paths = None, ylabel = 'Value'):
+        """
+        Plots the simulated paths of the GBM model.
+
+        Parameters
+        ----------
+        paths : optional
+            If specified, the function will plot the given paths. Otherwise, it simulates new paths.
+        ylabel : str, optional
+            Label for the y-axis. Default is 'Value'.
+        """
         plot_simulated_paths(self._t, self.simulate, paths, title="Geometric Brownian Motion", ylabel=ylabel)
 
     @property
@@ -119,4 +175,3 @@ class GeometricBrownianMotion:
     def start_date(self, value: str) -> None:
         self._start_date = value
         self._t = generate_date_range(value, self._T, self._num_steps)
-
