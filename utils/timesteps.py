34f48f224019030af60a970665e53cc262a98e86
YOSRI BEN HALIMA <yosri.benhalima@ept.ucar.tn>
2024-09-18 09:37:17 +0100

    Update timesteps

--- utils/timesteps.py
+++ utils/timesteps.py
@@ -4,23 +4,24 @@ def generate_date_range(start_date: str, T: float, num_steps: int) -> pd.Datetim
     """
     Generates a date range from the start date with a duration of T years and num_steps number of steps.
 
-    ## Parameters
+    Parameters
+    ----------
+    start_date : str
+        The start date in 'YYYY-MM-DD' format.
+    T : float
+        The duration of the date range in years.
+    num_steps : int
+        The number of dates in the date range.
 
-    - start_date (str): The start date in 'YYYY-MM-DD' format.
-    - T (float): The duration of the date range in years.
-    - num_steps (int): The number of dates in the date range.
-
-    ## Returns
-
-    - pd.DatetimeIndex: A Pandas DatetimeIndex containing the date range.
+    Returns
+    -------
+    pd.DatetimeIndex
+        A Pandas DatetimeIndex containing the date range.
     """
-    # Convert start_date to a datetime object
+
     start = pd.to_datetime(start_date)
-    
-    # Calculate the end date by adding T years to the start date
     end = start + pd.DateOffset(years=T)
     
-    # Generate the date range with num_steps dates
     date_range = pd.date_range(start=start, end=end, periods=num_steps)
     
     return date_range
