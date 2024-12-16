6e394a92577c3acedded77ec5470cdc774ab5a88
Yosri Ben Halima <yosri@laevitas.ch>
2024-12-16 23:58:42 +0100

    refactor(heston): update return type annotation for simulate_paths

--- FinStoch/processes/heston.py
+++ FinStoch/processes/heston.py
@@ -148,7 +148,7 @@ class HestonModel:
 
         Returns
         -------
-        np.ndarray
+        Tuple[np.ndarray, np.ndarray]
             A tuple of two 2D arrays representing simulated asset prices and volatilities, where each row represents a simulated path.
             The first array has the shape (num_paths, num_steps) for process values (e.g. asset prices), and the second array has the same shape for volatilities.
         """
